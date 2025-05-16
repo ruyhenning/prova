705968c686d19bd2a8e16c1ae8a05516e561770e
ruyhenning <ruyzinmelo@gmail.com>
2025-05-16 14:45:38 -0300

    exercicios

--- backend/backend/backend/logicadeProgramaçao/05_exercicioscondicionais.py
+++ backend/backend/backend/logicadeProgramaçao/05_exercicioscondicionais.py
@@ -1,5 +1,36 @@
-nome = input("me diga seu nome")
+num1 = int(input("digite o valor:"))
+num2 = int(input("digite o valor:"))
+operacao = (input("digite a operacao : "))
+ 
+match operacao:
+    case "*":
+        soma = num1 * num2
+        print("soma", soma)
 
-print("gabriel vc é noia?", nome)
+idade = int(input("sua idade?"))
 
-numero = int(input(" <"))
+match idade:
+     
+    case x if x <12 :
+        print("crianca")
+    case x if x >=12 and x <18:
+        print("adolescente")
+    case x if x >=18 and x <60:
+        print("adulto")
+    case x if x >=60 and x <100:
+        print("idoso")
+    case x if x >=100 :
+        print("morto")  
+
+notas = int(input("sua nota?"))
+
+match notas:
+
+    case 10:
+      print("gabaritou")
+    case 7|8|9:
+      print("ta na media")
+    case 1|2|3|4|5|6:
+      print("reprovado huahauha")
+    case _:
+        print("nota invalida")
