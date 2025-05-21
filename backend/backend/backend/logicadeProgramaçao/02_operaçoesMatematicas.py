35ecb18ee7009ef174a6f701116ee11fbafdcad6
ruyhenning <ruyzinmelo@gmail.com>
2025-05-21 11:47:59 -0300

    iniciando programacao

---
+++ backend/backend/backend/logicadeProgramaçao/02_operaçoesMatematicas.py
@@ -0,0 +1,15 @@
+a=4987
+b=8576
+
+soma=a+b
+subtraçao= a-b
+multiplicaçao = a*b
+divisao= a/b
+ 
+print("soma dos produtos a + b:", soma)
+print("subtraçao dos produtos a - b para controle:", subtraçao)
+print("multiplacaçao dos produtos a * b para aumento de carga:", multiplicaçao)
+print("divisao dos produtos a / b para separaçao de carga:", divisao)
+
+
+
