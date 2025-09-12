1663d9d6a816c93327524a092d90e67f46eddd4e
ruyhhenning <ruyzinmelo@gmail.com>
2025-09-12 08:44:29 -0300

    atividade avaliativa

--- backend/backend/backend/logicadeProgramaçao/atividadeavaliativa.py
+++ backend/backend/backend/logicadeProgramaçao/atividadeavaliativa.py
@@ -1,30 +1,33 @@
 import json
 
 ARQUIVO_PERIFERICOS = 'perifericos.json'
-ARQUIVOS_CATEGORIA = ' categoria'
+ARQUIVOS_CATEGORIAS = ' categoria'
 
-categoria= []
+categorias= []
 perifericos = []
 
-id_categoria = 1
+id_categorias = 1
 id_perifericos = 1
 
 def carregar_arquivo(perifericos):
 
     try :
-        with open (categoria.json,'r') as arquivo:
-            return json.load(arquivo)
+            with open ('categorias.json','r') as arquivo:
+                return json.load(arquivo)
     except FileNotFoundError:
-        return []
-    
+            return []
+        
 def salvar_arquivo(nome, dados):
     with open(nome,'w') as arquivo:
-        json.dump(dados,arquivo, indent=4)
+            json.dump(dados,arquivo, indent=4)
 
     try:
-        escolha = int(input("digite sua escolha: "))
+            escolha = int(input("digite sua escolha: "))
 
-def ixibir_menu():
+    except FileNotFoundError:
+     print("somentes numeros")
+
+def exibir_menu():
     print("\n----perifericos----")
     print("1)cadastrar perifericoss")
     print("2)cadastrar categoria")
@@ -32,26 +35,76 @@ def ixibir_menu():
     print("4)listar perifericos")
     print("[5]sair")
 
+
+def cadastrar_categoria():
+    global id_categorias
+    global categorias
+
+    nome_categoria = input("nome categoria: ")
+    categoria = {"id": id_categorias, "nome": nome_categoria}
+    categorias.append(categoria)
+
+    salvar_arquivo(ARQUIVOS_CATEGORIAS,categorias)
+    id_categorias += 1
+
+    print("categoria cadastrada com sucesso !!")
+
 def cadastrar_perifericos():
     global id_perifericos
     global perifericos
-    nome_perifericos = input("nome do perifericos: ")
+    nome_perifericos = input("nome dos perifericos: ")
     preco = float (input("preço: "))
-    id_categoria = int(input("ID da categoria: "))
+    id_categorias = int(input("ID das categorias: "))
     perifericos = {
 
-        "id": perifericos_id,
+        "id": id_perifericos,
         "nome": nome_perifericos,
-        "id_categoria": id_categoria
+        "id_categorias": id_categorias
     }
     perifericos.append(perifericos)
     salvar_arquivo(ARQUIVO_PERIFERICOS)
-    perifericos_id+= 1
+    id_perifericos+= 1
 
-def cadastrar_categoria(categoria_id):
-    nome_categoria = input("nome categoria: ")
-    categoria_id+= 1
-    categoria = {"id": categoria_id, "nome": nome_categoria}
-    categoria.append(categoria)
-    salvar_arquivo(ARQUIVOS_CATEGORIA, categoria)
-    print("categoria cadastrada com sucesso! ")
+    print ("periferico cadastrado com sucesso !!")
+
+def listar_categorias():
+    global categorias
+    if categorias:
+        print("CATEGORIAS:")
+        for categorias in categorias:
+            print(f"ID: {categorias['id']} - Nome: {categorias['nome']}")
+    else:
+        print("Nenhuma categoria cadastrada.")
+
+def listar_perifericos():
+    global perifericos
+    global categorias
+    if perifericos:
+        print("Perifericos:")
+        for perifericos in perifericos:
+            print(f"ID: {perifericos['id']} - Nome: {perifericos['nome']} - Preço: {perifericos['preco']} - Categoria: {perifericos['id_categoria']}") 
+    else:
+        print("sem perifericos cadastrado.")
+
+categorias = carregar_arquivo(ARQUIVOS_CATEGORIAS)
+perifericos = carregar_arquivo(ARQUIVO_PERIFERICOS)
+
+while True:
+    exibir_menu()
+    opcao = input("Escolha: ")
+
+    if opcao == '1':
+        cadastrar_categoria()
+    elif opcao == '2':
+        cadastrar_perifericos()
+    if opcao == '3':
+        listar_categorias()
+    elif opcao == '4':
+        listar_perifericos()
+    elif opcao == '5':
+        print("Saindo... Até logo!")
+        break
+    else:
+        print("Opção inválida.")
+
+listar_perifericos()
